c7b1ddec59ff267cf3ff42cc8a1a362e57533bd0
João Pablo <jp.pablolima@gmail.com>
2021-03-15 11:35:56 -0300

    servidor

---
+++ sizify/task.py
@@ -0,0 +1,20 @@
+from urllib.request import urlopen
+import urllib.request
+from bs4 import BeautifulSoup
+from urllib.error import HTTPError
+from urllib.error import URLError
+
+# http://35.237.182.83:3000/users/sign_in
+
+try:
+    url = urlopen("http://35.237.182.83:3000/users/sign_in")
+except HTTPError as erro:
+    print(erro)
+except URLError:
+    print('Servidor inativo')
+else:
+    res = BeautifulSoup(url.read(),"html5lib")
+    if res.input is None:
+        print('Login')
+    else:
+        print(res.input,'Servidor Ativo')
